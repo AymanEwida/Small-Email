227809244ba828311fced2a84f4425628ed9f98c
AymanEwida <ayman25a.05@gmail.com>
2023-07-17 18:50:05 +0300

    adding more design to send email component

--- client/src/components/send-email/SendEmail.tsx
+++ client/src/components/send-email/SendEmail.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 
 import { AiOutlineFullscreen, AiFillDelete, AiOutlineFullscreenExit, AiOutlineDeliveredProcedure } from 'react-icons/ai';
+import { TiDelete } from 'react-icons/ti';
+import { BiImageAdd } from 'react-icons/bi';
+import { FiLink2 } from 'react-icons/fi';
+import { MdOutlineAttachFile } from 'react-icons/md';
+import { ImTextColor } from 'react-icons/im';
 
 import Icon from '../icon/Icon';
 import Button from '../button/Button';
@@ -80,6 +85,21 @@ const SendEmail: React.FC<SendEmailProps> = ({ closeSendEmail }) => {
           value={toss}
           customFunc={(event: Event<InputElement>) => setToss(event.target.value)} 
           />
+          {/* <div 
+           style={{ minHeight: '196px', maxHeight: '320px' }} 
+           className='h-72 outline-none focus:border-none px-1' 
+           role='textbox' 
+           aria-multiline="true" 
+           tabIndex={1} 
+           aria-label='body-content'
+           contentEditable="true" 
+           aria-controls=':tp' 
+           aria-owns=':tp' 
+           spellCheck='false'
+           onClick={(e) => console.log(e)}
+          >
+            <br />
+          </div> */}
           <textarea 
           cols={30} 
           rows={10}
@@ -90,6 +110,21 @@ const SendEmail: React.FC<SendEmailProps> = ({ closeSendEmail }) => {
           onChange={(event: Event<TextAreaElement>) => setTos(event.target.value)}
           />
         </div>
+        {/* <div className='px-3 py-2 flex flex-row gap-3 overflow-x-auto'>
+          <div className='flex items-center pr-2 rounded-full bg-blue-500'>
+            <Icon
+             title='Delete'
+             textSize='2xl'
+             iconPosition='left'
+             icon={<TiDelete />}
+             color='white'
+             bgColor='bg-transparent' 
+            />
+            <p className='text-gray-200'>
+              index.pptx
+            </p>
+          </div>
+        </div> */}
         <div className='flex justify-between items-center px-4 py-2'>
           <Button
           type='submit'
@@ -100,13 +135,48 @@ const SendEmail: React.FC<SendEmailProps> = ({ closeSendEmail }) => {
           paddingSize='2'
           color='white' 
           />
+          <div className='flex items-center'>
+            <Icon
+             title='Design Possibilities'
+             iconPosition='top'
+             color='white'
+             textSize='md'
+             bgColor='bg-gray-400'
+             icon={<ImTextColor />}
+            />
+            <Icon
+             title='Add Image'
+             iconPosition='top'
+             color='white'
+             textSize='md'
+             bgColor='bg-gray-400'
+             icon={<BiImageAdd />}
+            />
+            <Icon
+             title='Add Link'
+             iconPosition='top'
+             color='white'
+             textSize='md'
+             bgColor='bg-gray-400'
+             icon={<FiLink2 />}
+            />
+            <Icon
+             title='Add File'
+             iconPosition='top'
+             color='white'
+             textSize='md'
+             bgColor='bg-gray-400'
+             icon={<MdOutlineAttachFile />}
+            />
+          </div>
           <Icon
-          title='Remove'
-          iconPosition='top'
-          color='white'
-          bgColor='bg-gray-400'
-          icon={<AiFillDelete />}
-          customFunc={closeSendEmail}
+           title='Remove'
+           iconPosition='top'
+           color='white'
+           textSize='md'
+           bgColor='bg-gray-400'
+           icon={<AiFillDelete />}
+           customFunc={closeSendEmail}
           />
         </div>
       </form>
