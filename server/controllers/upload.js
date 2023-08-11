783314dda7fe5192013077ef03f0ab20e24cbe3e
AymanEwida <ayman25a.05@gmail.com>
2023-08-11 17:26:50 +0300

    providing the ability to upload file and video

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -13,20 +13,60 @@ async function uploadImage (req, res) {
     }
 
     const result = await cloudinary.uploader.upload(
-        req.files.image.tempFilePath,
+        image.tempFilePath,
         {
-          use_filename: true,
-          folder: 'Small-Email',
+            use_filename: true,
+            folder: 'Small-Email',
+            resource_type: 'image'
         }
     );
 
     res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 }
 
+async function uploadVideo (req, res) {
+    if (!req.files) {
+        throw new BadRequestError('No video uploaded.');
+    }
+
+    const video = req.files.video;
+    if (!video.mimetype.startsWith('video')) {
+        throw new BadRequestError('Please upload an video.');
+    }
+
+    const result = await cloudinary.uploader.upload(
+        video.tempFilePath,
+        {
+          use_filename: true,
+          folder: 'Small-Email',
+          resource_type: 'video'
+        }
+    );
+
+    res.status(StatusCodes.OK).json({ video: { src: result.secure_url } });
+}
+
 async function uploadFile (req, res) {
-    res.status(StatusCodes.OK).json({ msg: "Hello" });
+    if (!req.files) {
+        throw new BadRequestError('No image uploaded.');
+    }
+
+    const file = req.files.file;
+
+    const result = await cloudinary.uploader.upload(
+        file.tempFilePath,
+        {
+          use_filename: true,
+          folder: 'Small-Email',
+          resource_type: 'raw'
+        }
+    );
+
+    res.status(StatusCodes.OK).json({ file: { src: result.secure_url, filename: file.name } });
 }
 
 module.exports = {
-    uploadImage
+    uploadImage,
+    uploadVideo,
+    uploadFile
 }
--- server/routes/upload.js
+++ server/routes/upload.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    uploadImage
+    uploadImage,
+    uploadVideo,
+    uploadFile
 } = require('../controllers/upload');
 
-router.route('/image').post(uploadImage)
+router.route('/image').post(uploadImage);
+router.route('/video').post(uploadVideo);
+router.route('/file').post(uploadFile);
 
 module.exports = router;
